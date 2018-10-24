0f47353bbd72fcd63820579dbf567ea947f9973e
Blanton, Taylor <matthewtblanton@gmail.com>
2018-10-23 23:40:36 -0500

    added changelog entry for Enh ABC

--- src/parser/shaman/enhancement/CHANGELOG.js
+++ src/parser/shaman/enhancement/CHANGELOG.js
@@ -1,8 +1,13 @@
 // import React from 'react';
 
-import { HawkCorrigan } from 'CONTRIBUTORS';
+import { HawkCorrigan, mtblanton } from 'CONTRIBUTORS';
 
 export default [
+  {
+    date: new Date('2018-10-19'),
+    changes: `Added 'Always be casting' and 'Preparation checker' to the Enhancement checklist`,
+    contributors: [mtblanton]
+  }
   {
     date: new Date('2018-09-16'),
     changes: 'Updated Enhancement Shaman for BfA.',
